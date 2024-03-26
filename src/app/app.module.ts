1a3ca5cf604d21ef9a387cc242ba48ee91699df5
Wanjiku Njuguna <117892624+njugunaemilly@users.noreply.github.com>
2024-03-26 10:30:12 +0300

    Create component to disaply all tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllTasksComponent } from './all-tasks/all-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllTasksComponent
   ],
   imports: [
     BrowserModule,
